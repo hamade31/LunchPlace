9cccad8311dd722fd1377d299841dc541cf56271
hamade31 <mohammadabouchama@gmail.com>
2021-08-14 14:23:35 +0400

    Setup django_rest

--- lunchplace/urls.py
+++ lunchplace/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from people import views
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
